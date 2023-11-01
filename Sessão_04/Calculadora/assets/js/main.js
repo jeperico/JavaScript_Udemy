7ddcf05abbdef2ccbc73477ca62f68366b8fc0ee
João Périco <jeperico@terra.com.br>
2023-11-01 14:49:02 -0300

    Calculadora

--- Sessão_04/Calculadora/assets/js/main.js
+++ Sessão_04/Calculadora/assets/js/main.js
@@ -1,15 +1,66 @@
-function loadPage() {
+function createCalculator() {
+    return {
+        display: document.querySelector('#display'),
 
-    $('.textInput').removeClass('active');
+        start() {
+            this.clickButton();
+            this.keyPressEnter();
+        },
 
-    $('.textInput').on('focus', function () {
-        $('.textInput').removeClass('active');
-        $(this).addClass('active');
-    })
+        clickButton() {
+            document.addEventListener('click', function (e) {
+                const element = e.target;
+                if (element.classList.contains('buttonNumber')) {
+                    this.buttonToDisplay(element.innerText);
+                }
+                if (element.classList.contains('buttonClear')) {
+                    this.clearDisplay();
+                }
+                if (element.classList.contains('buttonDelete')) {
+                    this.deleteOne();
+                }
+                if (element.classList.contains('buttonEqual')) {
+                    this.mathCount();
+                }
+            }.bind(this))
+        },
+        
+        keyPressEnter() {
+            this.display.addEventListener('keyup', (e) => {                
+                if(e.keyCode === 13){
+                    this.mathCount();
+                }
+            })
+        },
 
-    $('.textInput').on('blur', function () {
-        $('.textInput').removeClass('active');
-    });
+        deleteOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        mathCount() {
+            let count = this.display.value;
+            try {
+                count = eval(count);
+                
+                if (!count) {
+                    alert('Conta inválida!')
+                    return;
+                }
+                this.display.value = String(count);
+            } catch (e) {
+                alert('Conta inválida!')
+            }
+        },
+
+        buttonToDisplay(value) {
+            this.display.value += value;
+        }
+    };
 }
 
-loadPage();
+const calculator = createCalculator();
+calculator.start();
