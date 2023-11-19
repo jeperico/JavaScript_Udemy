5081b6c4fce2b24cb1902c719dd55c67e36d58e8
jeperico <jeperico@terra.com.br>
2023-11-19 19:24:24 -0300

    Arrays

--- Sessão_05/Array.js
+++ Sessão_05/Array.js
@@ -1,8 +1,14 @@
-const nomes = ['Eduardo', 'Maria', 'Joana'];
-const nomes2 = new Array('Eduardo', 'Maria', 'Joana');
-const novo = [...nomes]; // spread operator
-nomes[2] = 'Matheus';
-delete nomes[2];
-novo.pop();
-console.log(nomes);
-console.log(novo);
+// const nomes = ['Eduardo', 'Maria', 'Joana'];
+// const nomes2 = new Array('Eduardo', 'Maria', 'Joana');
+// const novo = [...nomes]; // spread operator
+// nomes[2] = 'Matheus';
+// delete nomes[2];
+// novo.pop();
+// console.log(nomes);
+// console.log(novo);
+
+const nomes = ['Luiz', 'Otávio', 'Miranda'];
+const nome = nomes.join(' ');
+const nomes2 = 'Luiz Otávio Miranda';
+const nome2 = nomes2.split(' ');
+console.log(nome);
