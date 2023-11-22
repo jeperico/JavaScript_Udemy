e5e5a5f1ad9d6ecb34fe6a21ec45f53f0097b17b
jeperico <jeperico@terra.com.br>
2023-11-22 14:04:00 -0300

    Filter

--- Sessão_05/Contatenacao.js
+++ Sessão_05/Contatenacao.js
@@ -2,4 +2,4 @@ const a1 = [1, 2, 3];
 const a2 = [4, 5, 6];
 const a3 = a1.concat(a2, [7, 8, 9], 'Lúcio');
 console.log(a3);
-// ... rest
+// ... rest  ou ... spread
---
+++ Sessão_05/Filter.js
@@ -0,0 +1,35 @@
+// Filter -> Sempre retornará um Array com a mesma quantidadde de elementos ou menos;
+
+const numbers = [5, 50, 80, 1, 2, 3, 5, 7, 8, 11, 15, 22, 27, 52, 8, 10.1];
+
+// function confere(Array) {
+//     arrayResult = [];
+//     for(i of Array){
+//         if(i > 10){
+//             arrayResult.push(i)
+//         }
+//     }
+//     return arrayResult;
+// }
+// console.log(confere(numbers));
+
+// function callbackFilter(valor, indice, array) {
+
+const numbersFilter = numbers.filter(value => value > 10);
+console.log(numbersFilter);
+
+//
+
+const people = [
+    {nome: 'Anna', idade: 17},
+    {nome: 'Ana', idade: 50},
+    {nome: 'Aline', idade: 94},
+    {nome: 'Angélica', idade: 9},
+    {nome: 'Amanda', idade: 25},
+    {nome: 'Alessandra', idade: 12},
+];
+
+const pBigName = people.filter(value => value.nome.length > 5);
+const pOldage = people.filter(value => value.idade > 50);
+const pLetterA = people.filter(value => value.nome[0] === "A");
+console.log(pBigName, pOldage, pLetterA);
