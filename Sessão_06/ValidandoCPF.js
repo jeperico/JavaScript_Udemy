bafed96aed6100579e879a8463a4367e665a850f
jeperico <jeperico@terra.com.br>
2024-02-05 00:06:45 -0300

    Ultra site verificador de CPF válido

--- Sessão_06/ValidandoCPF.js
+++ Sessão_06/ValidandoCPF.js
@@ -1,23 +1,40 @@
 // const originalCFP = "705.484.450-52";
-const originalCFP = "070.987.720";
-let modifiedCPF = originalCFP.slice(0, 12).replace(/\D+/g, ''); // 705484450   // /\D+/g representa tudo que não é um número
 
-function calcNumber(t) {
-    if(typeof modifiedCPF === `undefined` || modifiedCPF.length !== t) return;
-
-    constant = modifiedCPF.length + 1;
-    acumulator = 0;
-    for (i of modifiedCPF) {
-        acumulator += i * constant;
-        constant--;
+function crica() {
+    
+    let boxV = document.querySelector('#textBox');
+    let box = boxV.value;
+    if (box.length !== 14) {
+        console.log('Valor inválido');
+        return;
     }
-    const number = 11 - (acumulator % 11) > 9 ? 0 : 11 - (acumulator % 11);
-    modifiedCPF = modifiedCPF.concat(number);
-    return number + '';
+    const originalCFP = box;
+    let modifiedCPF = originalCFP.slice(0, 12).replace(/\D+/g, ''); // 705484450   // /\D+/g representa tudo que não é um número
+
+    function calcNumber(t) {
+        if (typeof modifiedCPF === `undefined` || modifiedCPF.length !== t) return;
+
+        constant = modifiedCPF.length + 1;
+        acumulator = 0;
+        for (i of modifiedCPF) {
+            acumulator += i * constant;
+            constant--;
+        }
+        const number = 11 - (acumulator % 11) > 9 ? 0 : 11 - (acumulator % 11);
+        modifiedCPF = modifiedCPF.concat(number);
+        return number + '';
+    }
+
+    const firstNumber = calcNumber(9);
+    const secondNumber = calcNumber(10);
+
+    const div = document.querySelector('#div');
+    if (firstNumber === originalCFP[12] && secondNumber === originalCFP[13]) {
+        console.log(`O CPF ${originalCFP} é valido`)
+        div.innerHTML = `O CPF ${originalCFP} é valido`;
+    } else {
+        console.log(`O CPF ${originalCFP} é invalido`)
+        div.innerHTML = `O CPF ${originalCFP} é invalido`;
+    }
+
 }
-
-const firstNumber = calcNumber(9);
-const secondNumber = calcNumber(10);
-
-if (firstNumber === originalCFP[12] && secondNumber === originalCFP[13]) console.log(`O CPF ${originalCFP} é valido`);
-else console.log(`O CPF ${originalCFP} é invalido`);
